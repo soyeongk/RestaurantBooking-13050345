c435e626c9c7648da5e0b8b9358d21d46390f2b5
soyeongk <soyeong_k@naver.com>
2026-07-02 13:02:56 +0900

    [feature] test case 9

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -10,6 +10,7 @@ from test_communication import TestableSmsSender, TestableMailSender
 NOT_ON_THE_HOUR = datetime.strptime('2021/03/26 09:05', '%Y/%m/%d %H:%M')
 ON_THE_HOUR = datetime.strptime('2021/03/26 09:00', '%Y/%m/%d %H:%M')
 CUSTOMER = Customer("Fake name", "010-1234-5678")
+CUSTOMER_WITH_MAIL = Customer("Fake name", "010-1234-5678", "test@test.com")
 
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
@@ -75,8 +76,14 @@ def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler)
 
     assert mail_sender.send_mail_count == 0
 
-def test_이메일이_있는_경우에는_이메일_발송():
-    pass
+def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler):
+    mail_sender = TestableMailSender()
+    booking_scheduler.set_mail_sender(mail_sender)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
+
+    booking_scheduler.add_schedule(schedule)
+
+    assert mail_sender.send_mail_count == 1
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
     pass
